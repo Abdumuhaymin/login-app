54c45db3e2e8d7272a43ad33517107a7ff81c725
uzbfamily57@gmail.com <abdurahmonov06a@gmail.com>
2024-03-08 08:13:05 +0500

    hello

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { request } from "../../config/confog";
+import Cookies from "js-cookie";
 export const Home = () => {
+  const [data, setData] = React.useState([]);
+  request
+    .get("/users", `Bearer ${Cookies.get("server-token")}`)
+    .then((res) => setData(res.data.user));
   return (
-    <div>
-      <h1>
-        home
-      </h1>
-    </div>
+    <>
+      {data.map((item) => (
+        <div>
+          <h1>{item.name}</h1>
+          <h2>{item.Surname}</h2>
+        </div>
+      ))}
+    </>
   );
 };
 
